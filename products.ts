d54da0f87d7fc0c51d045cccac6b3d5c6232d05a
Roma796 <78651897+Roma796@users.noreply.github.com>
2021-02-24 15:03:20 +0600

    lab4

---
+++ products.ts
@@ -0,0 +1,94 @@
+export const products = [
+    { 
+      id: 1,
+      name: 'iPhone 12 Pro Max',
+      price: '$1099.99',
+      rating: '10/10',
+      description: 'iPhone 12 Pro Max, 128GB, Graphite',
+      img: "https://m.media-amazon.com/images/I/71XXJC7V8tL._FMwebp__.jpg",
+      link:'https://www.amazon.com/dp/B08L5R1CCC/ref_=vn_s_iwp_0?pd_rd_w=74xZ9&pf_rd_p=84cbcc8f-0fe2-44fc-ae2a-87f8b101dbca&pf_rd_r=4HWEFSE48NJ3M4VSXETX&pd_rd_r=86383384-9541-4b19-89a0-d9be5afa67f2&pd_rd_wg=VqkTF&qid=1614036067#'
+    },
+    {
+      id: 2,
+      name: 'iPhone 12 Pro',
+      price: '$830.99',
+      rating: '9.5/10',
+      description: 'iPhone 12 Pro, 128GB, Pacific Blue',
+      img: 'https://m.media-amazon.com/images/I/71DVgBTdyLL._FMwebp__.jpg',
+      link: 'https://www.amazon.com/dp/B08L5NRZNW/ref=fs_a_iwp2_1#'
+    },
+    {
+      id: 3,
+      name: 'iPhone 12 Mini',
+      price: "$699.99",
+      rating: '9.9/10',
+      description: 'iPhone 12 Mini, 64GB, RED',
+      img:'https://m.media-amazon.com/images/I/71Sa8+9H3xL._FMwebp__.jpg',
+      link: "https://www.amazon.com/dp/B08L5RJ392/ref_=vn_s_iwp_3?pd_rd_w=tjKfy&pf_rd_p=84cbcc8f-0fe2-44fc-ae2a-87f8b101dbca&pf_rd_r=ET81HRE554ESXGX1RABA&pd_rd_r=74e26584-58ca-4849-a861-7ed6c3257c43&pd_rd_wg=ue4yx&qid=1614102361#"
+    },
+    {
+      id: 4,
+      name: 'iPhone SE',
+      price: '$279.99',
+      rating: '9/10',
+      description: 'iPhone SE, 64GB, Black',
+      img: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-se-white-select-2020_GEO_EMEA?wid=834&hei=1000&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1586574260599',
+      link: 'https://www.amazon.com/dp/B0875RT5WS/ref=fs_a_iwp2_4'
+    },
+    {
+      id: 5,
+      name: 'iPhone 11 Pro Max',
+      price: "$999.99",
+      rating: '9.9/10',
+      description: 'iPhone 11 Pro Max, 64GB, Midnight Green',
+      img:'https://m.media-amazon.com/images/I/81j2j9x-KWL._FMwebp__.jpg',
+      link: "https://www.amazon.com/dp/B0844JKGSK/ref_=vn_s_iwp1_0?pd_rd_w=eAkes&pf_rd_p=61c17cb9-4bb8-40ea-b5b5-77e848e967a8&pf_rd_r=GBQ64CRA8GQKM223B09Z&pd_rd_r=15a0ea01-030c-4f5b-b005-1a00c09baf2b&pd_rd_wg=R74I7&qid=1614105361"
+    },
+    {
+      id: 6,
+      name: 'iPhone 11 Pro',
+      price: '$999.99',
+      rating: '9.5/10',
+      description: 'iPhone 11 Pro, 64GB, Space Gray',
+      img: 'https://m.media-amazon.com/images/I/81HSzsIkJdL._FMwebp__.jpg',
+      link: 'https://www.amazon.com/dp/B0844DSHY4/ref_=vn_s_iwp1_1?pd_rd_w=R5gkn&pf_rd_p=61c17cb9-4bb8-40ea-b5b5-77e848e967a8&pf_rd_r=ZJ2FJSC12F9JS7K115ZE&pd_rd_r=e59940a2-794c-4db2-b2ca-286027d416dd&pd_rd_wg=4sHrI&qid=1614105639#'
+    },
+    {
+      id: 7,
+      name: 'iPhone 11',
+      price: '$599,00',
+      rating: '9.55/10',
+      description: 'iPhone 11, 64GB, Black',
+      img: 'https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/d/c/dc242a035e967046ae752af9e234b5d8b7478357_178269_1dfgh.jpg',
+      link: 'https://www.amazon.com/dp/B0844NJJF3/ref=fs_a_iwp2_5#'
+    },
+    {
+      id: 8,
+      name: 'iPhone XS Max',
+      price: '$999.99',
+      rating: '8/10',
+      description: 'iPhone XS Max, 64GB, Gold',
+      img: 'https://www.friendlymobiles.com/wp-content/uploads/2020/07/iPhone-XS-Max_Gold-1.png',
+      link: 'https://www.amazon.com/dp/B07VMNWWWZ/ref_=vn_s_iwp1_3?pd_rd_w=R5gkn&pf_rd_p=61c17cb9-4bb8-40ea-b5b5-77e848e967a8&pf_rd_r=ZJ2FJSC12F9JS7K115ZE&pd_rd_r=e59940a2-794c-4db2-b2ca-286027d416dd&pd_rd_wg=4sHrI&qid=1614105639#'
+    },
+    { 
+      id: 9,
+      name: 'iPhone XR',
+      price: '$319.00',
+      rating: '8.5/10',
+      description: ' iPhone XR, 64GB, Black',
+      img: "https://www.technodom.kz/media/catalog/product/b/a/bab5d8675425c9ea6a0e5f575083bd3ccf5cfe93_15662199504926.jpg",
+      link:'https://www.amazon.com/dp/B07VPRFYNW/ref=fs_a_iwp2_6#'
+    },
+    
+    {
+      id: 10,
+      name: 'iPhone XS',
+      price: '$499,00',
+      rating: '8/10',
+      description: 'iPhone XS, 64GB, Gold',
+      img: 'https://www.friendlymobiles.com/wp-content/uploads/2020/04/iPhone-XS_Gold.png',
+      link: 'https://www.amazon.com/dp/B07VMNXRKS/ref_=vn_s_iwp1_4?pd_rd_w=R5gkn&pf_rd_p=61c17cb9-4bb8-40ea-b5b5-77e848e967a8&pf_rd_r=ZJ2FJSC12F9JS7K115ZE&pd_rd_r=e59940a2-794c-4db2-b2ca-286027d416dd&pd_rd_wg=4sHrI&qid=1614105639#'
+    }
+
+  ];  
